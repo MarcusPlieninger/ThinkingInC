14f2886a9271dfd5d7a9a25d7ff31709424e28a6
MarcusPlieninger <mwp2101@columbia.edu>
2020-01-17 07:03:03 -0500

    finish chap4

---
+++ chap4/branch2switch/branch2switch.c
@@ -0,0 +1,35 @@
+/* branch2.c: Branching on arbitrary input */
+/* Finds first odd numer whose digits
+   add to 7. Assumes 2 digits only.*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    int i;
+
+    puts("I will find the first odd number whose digits add to 7!");
+
+    do {
+        int dig1, dig2;
+
+        printf("Enter a number: ");
+        fflush(stdout);
+        scanf("%d", &num);
+
+        switch (num%2) {
+            case 0:
+                break;
+            case 1:
+                dig2 = num%10;
+                dig1 = num/10;
+                if (dig1 + dig2 == 7) {
+                    printf("found %d\n", num);
+                    break;
+                }
+        }
+    }
+    while (num != 0);
+    
+    return 0;
+}
