3e3ac40c56961cf6806d4f3c4c02f4edc71af670
Marcus Plieninger <mwp2101@columbia.edu>
2020-01-19 12:16:43 -0500

    finish chap5 examples

--- chap5/struct/struct.c
+++ chap5/struct/struct.c
@@ -3,21 +3,20 @@
 #include <string.h>
 
 struct Hitter {
-    char last [16];     /* 15 + 1 */
+    char last[16];     /* 15 + 1 */
     char first[11];     /* 10 + 1 */
     int home_runs;
 };
 
 int main () {
-    struct Hitter h1 = {"McGwire," "Mark", 70};
+    struct Hitter h1 = {"McGwire", "Mark", 70};
     struct Hitter h2;
     strcpy(h2.last, "Sosa");
     strcpy(h2.first, "Sammy");
     h2.home_runs = h1.home_runs - 4;
+    printf("#1 == {%s, %s, %d}\n",
+           h1.last, h1.first, h1.home_runs);
+    printf("#2 == {%s, %s, %d}\n",
+           h2.last, h2.first, h2.home_runs);
+    return 0;
 }
-
-printf("#1 == {%s, %s, %d}\n",
-        h1.last, h1.first, h1.home_runs);
-printf("#2 == {%s, %s, %d}\n",
-        h2.last, h2.first, h2.home_runs);
-return 0;
