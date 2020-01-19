e7d3a97d5daeb3d2aa3e45dd6d766fea7d00566f
Marcus Plieninger <mwp2101@columbia.edu>
2020-01-19 18:13:04 -0500

    finish chap5 exercise

--- chap5/lab5/lab5.c
+++ chap5/lab5/lab5.c
@@ -11,7 +11,7 @@ last name, print out the list of Employees. */
 #include <stdio.h>
 #include <string.h>
 
-#define MAXEMPS = 10
+#define MAXEMPS 10
 
 struct Employee {
     char last[16];
@@ -21,16 +21,35 @@ struct Employee {
 };
 
 int main() {
-    struct Employee emp[10];
+    struct Employee emps[10];
     int n, i;
 
     for (n = 0; n < MAXEMPS; ++n) {
-        printf("Please enter last name: ");
-        printf("Please enter first naame: ");
+        printf("Please enter last name: "); fflush(stdout);
+        gets(emps[n].last);
+
+        if (strlen(emps[n].last) == 0)
+            break;
+        
+        printf("Please enter first name: ");
+        fflush(stdout);
+        gets(emps[n].first);
         printf("Please enter title: ");
+        fflush(stdout);
+        gets(emps[n].title);
         printf("Please enter salary: ");
+        fflush(stdout);
+        scanf("%d", &emps[n].salary);
+        getchar(); /* eat newline */
     }
 
-
+    for (i = 0; i < n; ++i)
+        printf("{%s, %s, %s, %d}\n",
+                emps[i].last,
+                emps[i].first,
+                emps[i].title,
+                emps[i].salary);
+    
+    return 0;
 }
 
